4e8148850a2fb2e5ce1ea88f2c403b13d1766e56
konitanko <2530857975@qq.com>
2017-11-18 21:51:26 +0800

    MingXuan school

---
+++ BST/js/main.js
@@ -0,0 +1,37 @@
+//$(document).ready(function(){
+//
+//  
+//	$("#nav-main").sticky({
+//		topSpacing :75,
+//	});
+//
+//	$('#nav').onePageNav({
+//		currentClass: 'current',
+//		changeHash: false,
+//		scrollSpeed: 15000,
+//		scrollThreshold: 0.5,
+//		filter: '',
+//		easing: 'easeInOutExpo'
+//	});
+//
+//   $('#top-nav').onePageNav({
+//       currentClass: 'active',
+//       changeHash: true,
+//       scrollSpeed: 1200
+//  });
+//
+//
+//});
+
+//$(document).ready(function(){ 
+//			var main= $("#nav-main").offset().top; 
+//			var container=$("#container"); 
+//			$(window).scroll(function(){ 
+//				if($(this).scrollTop()>nav-main){ 
+//					navFix.addClass("container"); 
+//				} 
+//				else{ 
+//					navFix.removeClass("container"); 
+//				} 
+//				}) 
+//		});
